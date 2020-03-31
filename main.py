baf9f9ec461f1fbdb2087bc36a4cf97db98b2fed
Sami Heikkinen <zami.heikkinen@hotmail.com>
2020-03-31 19:45:37 +0300

    Worked on timer module

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 
-
 from data_structure.period import Period
 from modules.timer import TimerModule
 
@@ -12,8 +11,11 @@ class Monitor:
         result = timer.time_it()
         test_period = Period(result)
         date, work_time = test_period.get_period()
+        print()
         print(date)
-        print(work_time)
+        print("Worktime in minutes: {:.2f}".format(work_time / 60))
+        print("Worktime in hours: {:.2f}".format((work_time/60)/60))
+        print()
 
 
     if __name__=='__main__':
--- modules/timer.py
+++ modules/timer.py
@@ -1,42 +1,52 @@
 #! /usr/bin/python3
 
-
 import time
 
 
 class TimerModule:
 
-
-    state = False
-
-
+    
     """
-    Takes time until user gives string "stop" and pauses the timer when user gives "pause"
+    Takes time until user gives input to pause, continue or stop
     """
     def time_it(self):
         start_time = time.time()
         elapsed_time = 0
-        str = "start"
         state = True
+        print("Timer started!")
+        self.print_instructions()
         while True:
             str = input()
-            if str == "pause":
+
+            if (str == "0" or str == "h" or str == "help"):
+                self.print_instructions()
+            if (str == "1" or str == "p" or str == "pause"):
                 state = False
-                print("Timer paused")
+                self.print_timer_state("paused")
                 end_time = time.time()
                 elapsed_time += end_time - start_time
-                print(elapsed_time)
-            if str == "continue":
+            if ((str == "2" or str == "c" or str == "continue") and state == False):
                 state = True
-                print("Timer continued")
-                print(elapsed_time)
+                self.print_timer_state("continued")
                 start_time = time.time()
-            if str == "stop":
-                print("Timer stopped")
-                print(elapsed_time)
+            if (str == "3" or str == "s" or str == "stop"):
+                self.print_timer_state("stopped")
                 end_time = time.time()
                 break
+            
         if not state:
             return elapsed_time
         return end_time - start_time + elapsed_time
 
+
+    def print_instructions(self):
+        print("=" * 33)
+        print("| Help:\t\t0|h|help\t|")
+        print("| Pause:\t1|p|pause\t|")
+        print("| Continue:\t2|c|continue\t|")
+        print("| Stop:\t\t3|s|stop\t|")
+        print("=" * 33)
+
+
+    def print_timer_state(self, status):
+        print("Timer " + status)
