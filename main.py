9c75f319cfddb09747d4490962bf93991eab467c
Sami Heikkinen <zami.heikkinen@hotmail.com>
2020-04-01 19:20:32 +0300

    Found library for creating console menu

--- main.py
+++ main.py
@@ -3,25 +3,45 @@
 from data_structure.period import Period
 from modules.periods import Periods
 from modules.timer import TimerModule
+from consolemenu import ConsoleMenu
+from consolemenu.items import FunctionItem
 
+
+# DOCS: https://console-menu.readthedocs.io/en/latest/
 
 class Monitor:
 
+    
     def main():
-        periods_list = Periods()
+
         timer = TimerModule()
-        result = timer.time_it()
-        test_period = Period(result)
-        date, work_time = test_period.get_period()
-        periods_list.add_period(test_period)
-        # print()
-        # print(date)
-        # print(test_period)
-        # print()
-        print("Worktime in minutes: {:.2f}".format(work_time / 60))
-        print("Worktime in hours: {:.2f}".format((work_time/60)/60))
-        # print()
+
+        menu = ConsoleMenu("Test menu", "Test subtitle")
+
+        function_item = FunctionItem("Call a function", input, ["Enter some input"])
+
+        menu.append_item(function_item)
+
+        menu.show()
+
+        
+        # periods_list = Periods()
+        # timer = TimerModule()
+        # result = timer.time_it()
+        # test_period = Period(result)
+        # date, work_time = test_period.get_period()
+        # periods_list.add_period(test_period)
+        # # print()
+        # # print(date)
+        # # print(test_period)
+        # # print()
+        # print("Worktime in minutes: {:.2f}".format(work_time / 60))
+        # print("Worktime in hours: {:.2f}".format((work_time/60)/60))
+        # # print()
         # print(periods_list)
+
+
+   #def menu():
 
 
     if __name__=='__main__':
