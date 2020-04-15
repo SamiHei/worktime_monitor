15229c1fdc973147087365789e98ef373282e96c
Sami Heikkinen <zami.heikkinen@hotmail.com>
2020-04-15 14:42:19 +0300

    Main menu base working correctly

--- main.py
+++ main.py
@@ -21,7 +21,28 @@ class Monitor:
 
     def main(self):
         curses.wrapper(self.build_main_menu)
-        
+
+
+    '''
+    Basic settings at start
+    '''
+    def set_start_settings(self):
+        self.stdscr.clear()
+        curses.curs_set(0)
+        curses.noecho()
+        self.stdscr.keypad(True)
+        curses.start_color()
+
+
+    '''
+    Will be called at the end of the program
+    '''
+    def end_program(self):
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
 
     '''
     Base menu of the program contains all the other menus and functions
@@ -31,10 +52,10 @@ class Monitor:
         current_row_idx = 0
         menu = ["Timer", "Months", "Exit"]
 
+        self.set_start_settings()
         
         self.print_main_menu(menu, header, current_row_idx)
-        
-        
+
         while 1:
 
             key = self.stdscr.getch()
@@ -50,10 +71,7 @@ class Monitor:
                 self.stdscr.refresh()
                 self.stdscr.getch()
                 if (current_row_idx == (len(menu) - 1)):
-                    curses.nocbreak()
-                    stdscr.keypad(False)
-                    curses.echo()
-                    curses.endwin()
+                    self.end_program()
                     break
 
             self.print_main_menu(menu, header, current_row_idx)
@@ -62,46 +80,43 @@ class Monitor:
 
 
     '''
-    Commont method to print menu items to screen
+    Method to print menu items to screen
         menu = Array of menu items
+        menu_header = ascii graphic header
         current_row_idx = Selected row index
     '''
     def print_main_menu(self, menu, menu_header, current_row_idx):
-        curses.start_color()
+
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-
-        self.stdscr.clear()
         h, w = self.stdscr.getmaxyx()
-
-        curses.curs_set(0)
-        curses.noecho()
-        self.stdscr.keypad(True)
+        menu_y = h//2 - len(menu)//2
         
-        self.print_header(menu_header)
+        self.print_header(menu_header, menu_y)
 
         for idx, row in enumerate(menu):
             x = w//2
-            y = h//2 - len(menu)//2 + idx
+            y = menu_y + idx
             if (idx == current_row_idx):
                 self.stdscr.attron(curses.color_pair(1))
                 self.stdscr.addstr(y, x, row)
                 self.stdscr.attroff(curses.color_pair(1))
             else:
                 self.stdscr.addstr(y, x, row)
-                
 
         self.stdscr.refresh()
 
 
-    def print_header(self, menu_header):
-        curses.start_color()
+    '''
+    Method to print ascii graphic header to menu
+    '''
+    def print_header(self, menu_header, menu_y):
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
         h, w = self.stdscr.getmaxyx()
 
-        width = w//11
-        height = h//7
+        width = w//2 - 23
+        height = menu_y - 10
 
         self.stdscr.attron(curses.color_pair(2))
         for y, line in enumerate(menu_header.splitlines(), height):
@@ -121,8 +136,8 @@ class Monitor:
     '''
     def build_timer_view(self):
         print("Timer view")
-        
-             
+   
+
 if __name__ == '__main__':
     monitor = Monitor()
     monitor.main()
